49b0aac5f30ea325bda859c9056d9eab23c34831
danielebrugnara <brugnara.daniele@gmail.com>
2020-04-14 17:04:07 +0200

    Added working strip up time utility

--- Utilities/StripUptime.cxx
+++ Utilities/StripUptime.cxx
@@ -1,16 +1,18 @@
 #include<TFile.h>
+#include<TH2D.h>
 
+#include<iostream>
 #include<string>
 #include<vector>
 #include<map>
 
-StripUptime(std::string file_name){
+void StripUptime(std::string file_name){
 	TFile* file = new TFile(file_name.c_str());
 	std::vector<std::string> mg = {"MG1","MG3","MG4","MG5","MG7","MG11"};
 	std::vector<std::string> type = {"T","E"};
 	std::vector<std::string> strip = {"X","Y"};
-	std::map<std::string, std::map<std::string, std::vector<std::pair>>>> data;
-	//for(const auto &it_type: type){
+	std::map<std::string, std::map<std::string, std::map< std::string, std::vector<std::pair<int, int>>>>> data;
+	for(const auto &it_type: type){
 		for(const auto &it_mg: mg){
 			for(const auto &it_strip: strip){
 				std::string tmp_name = "pConf_SI_mStrip_"
@@ -19,7 +21,7 @@ StripUptime(std::string file_name){
 							+it_mg
 							+"_"
 							+it_strip;
-				TH2D* tmp_ptr = (TH2D*)file->Get(tmp_name);
+				TH2D* tmp_ptr = (TH2D*)file->Get(tmp_name.c_str());
 
 				for( int ii=0; ii<tmp_ptr->GetXaxis()->GetNbins(); ++ii){
 					double counts = 0;
@@ -28,16 +30,19 @@ StripUptime(std::string file_name){
 						double cont = tmp_ptr->GetBinContent(bin);
 						counts+=cont;
 					}	
-					data[it_type][it_mg][it_strip].push_back(counts);
+					data[it_type][it_mg][it_strip].push_back(std::make_pair(ii, counts));
 				}
 
 			}
 		}
-	//}
+	}
 	for(const auto &it_type: type){
-		for(const auto &it_mg: type){
+		for(const auto &it_mg: mg){
 			for(const auto &it_strip: strip){
-				for( const auto & counts: data[it_type][it_mg][it_strip]){
+				for( const auto & it_data: data[it_type][it_mg][it_strip]){
+					if (it_data.second<5){
+						std::cout << it_type << " " << it_mg << " " << it_strip <<  " "  << it_data.first << " "  <<it_data.second<<std::endl;
+					}
 							
 				}
 			}
